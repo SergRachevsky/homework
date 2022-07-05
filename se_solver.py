ce7d7cc9a52052e16dfd6f7837b05f66d5e586b4
Serg Rachevsky <radogor@gmail.com>
2022-07-06 02:31:46 +0300

    add more tests

--- se_solver.py
+++ se_solver.py
@@ -3,6 +3,11 @@
 import sys
 from math import sqrt
 
+RC_NOT_A_SE = -1
+RC_NO_ROOTS = 0
+RC_ONE_ROOT = 1
+RC_TWO_ROOTS = 2
+
 def get_input_data(args):
 
     # get arguments from command line (except sys.argv[0] which is a script name)
@@ -11,11 +16,18 @@ def get_input_data(args):
     # convert all arguments to integers (we expect they SHOULD be integers)
     args = list(map(int, args))
 
+    # we want number of arguments to be a multiple of three,
+    # so we add zeros for missed elements
+    while len(args) % 3:
+        args.append(0)
+
     # split list of arguments to list of triads
+#    triads = [args[i:i+3] for i in range(0, len(args), 3)]
+    
     return [args[i:i+3] for i in range(0, len(args), 3)]
 
 
-def make_sqare_equation(a, b, c):
+def make_se_text(a, b, c):
     # make square equation string more comfortable to read for humans
     a_str = f"{a}x^2" if abs(a) != 1 else "x^2"
     b_str = f"{' + ' if b > 0 else ' - '}{abs(b)}x" if b != 0 else ""
@@ -23,60 +35,48 @@ def make_sqare_equation(a, b, c):
 
     return f"{a_str}{b_str}{c_str} = 0"
 
-def make_text_solution(solution):
-    print(solution)
-    exit()
-    ...
 
 def se_solve(a, b, c):
     
-    solution = []
-    # calculate discriminant of suare equation
+    roots = []
+
+    # calculate discriminant of square equation
     d = b**2 - 4*a*c
 
     if a == 0:
-#        result = "Not a square equation (a = 0)"
-        solution.append(-1)
+        rc = -1
+        text = "Not a square equation (a=0)"
 
     elif d > 0:
+        rc = 2
         x1 = (-b + sqrt(d)) / (2 * a)
         x2 = (-b - sqrt(d)) / (2 * a)
-#        result = f"x1 = {x1:6.2f}; x2 = {x2:6.2f}"
-        solution = (2, x1, x2)
+        text = f"x1 = {x1:6.2f}; x2 = {x2:6.2f}"
+        roots.append(x1)
+        roots.append(x2)
 
     elif d == 0:
+        rc = 1
         x = -b / (2 * a)
-#        result = f"x  = {x:6.2f}"
-        solution.append(1, x)
+        text = f"x  = {x:6.2f}"
+        roots.append(x)
+    else:
+        rc = 0
+        text = "No roots"
 
-    else: 
-#        result = "No roots"
-        solution.append(0)
-
-    return solution
-
-
-def print_solution(a, b, c, roots):
-
-    square_equation = make_sqare_equation(a, b, c)
-    text_solution = make_text_solution(roots)
-
-#    se_triad_str = f"[{', '.join([f'{i:4}' for i in se_triad])}]"
-#    print(f"{se_triad_str:15}  ==>  {square_equation:24}  ==>  {roots}")
-    print(f"[{a:5}, {b:5}, {c:5}]  ==>  {square_equation:24}  ==>  {text_solution}")
+    return rc, roots, text
 
 
-    ...
+def print_solution(a, b, c, text):
+    square_equation = make_se_text(a, b, c)
+    print(f"[{a:5}, {b:5}, {c:5}]  ==>  {square_equation:24}  ==>  {text}")
 
 
 if __name__ == "__main__":
+
     se_triads = get_input_data(sys.argv)
 
-    for se_triad in se_triads:
+    for a, b, c in se_triads:
 
-        a = se_triad[0]
-        b = se_triad[1] if len(se_triad) > 1 else 0
-        c = se_triad[2] if len(se_triad) > 2 else 0
-
-        solution = se_solve(a, b, c)
-        print_solution(a, b, c, solution)
+        rc, roots, text = se_solve(a, b, c)
+        print_solution(a, b, c, text)
--- test_se_solver.py
+++ test_se_solver.py
@@ -2,26 +2,62 @@
 
 import unittest
 
-from se_solver import get_input_data, se_solve
+from se_solver import get_input_data, se_solve, make_se_text
+from se_solver import RC_NO_ROOTS, RC_NOT_A_SE, RC_ONE_ROOT, RC_TWO_ROOTS
+
 
 class TestSeSolver(unittest.TestCase):
     """Calc tests"""    
     def test_get_input_data(self):
-    
-        self.assertEqual(get_input_data(['hghg', '1', '2', '3', '4']), [[1, 2, 3], [4]], "Should be [[1, 2, 3], [4]]")
+        """
+        make sure arguments from command line are converted into the list of triads correctly
+        """
+        self.assertEqual(get_input_data(['hghg', '1', '2', '3', '4']), [[1, 2, 3], [4, 0, 0]], "Should be [[1, 2, 3], [4, 0, 0]]")
     
     
     def test_get_input_data_empty(self):
+        """
+        no arguments should lead to empty list of triads
+        """
         self.assertEqual(get_input_data(['hghg']), [], "Should be []")
+
+    def test_not_a_se(self):
+        """
+        if the first of the three is zero then result should be RC_NOT_A_SE
+        """
+        self.assertEqual(se_solve(0, 4, 5)[0], RC_NOT_A_SE, "Should be -1")
+
+    def test_no_roots(self):
+        """
+        check that we correctly solved the equation with no roots
+        """
+        self.assertEqual(se_solve(10, -1, 2)[0], RC_NO_ROOTS, "Should be 0")
+
+    def test_one_root(self):
+        """
+        check that we correctly solved the equation with one root
+        """
+        self.assertEqual(se_solve(-4, 12, -9)[0], RC_ONE_ROOT, "Should be 1")
+
+    def test_two_roots(self):
+        """
+        check that we correctly solved the equation with two roots
+        """
+        self.assertEqual(se_solve(2, 5, -2)[0], RC_TWO_ROOTS, "Should be 2")
+
+    def test_make_se_text(self):
+        """
+        check if we can costruct the equation text correctly using different variants of arguments
+        """
+        self.assertEqual(make_se_text(-1, 5, -2), "x^2 + 5x - 2 = 0", "Should be 'x^2 + 5x - 2 = 0'")
+        self.assertEqual(make_se_text(1, 5, -2), "x^2 + 5x - 2 = 0", "Should be 'x^2 + 5x - 2 = 0'")
+        self.assertEqual(make_se_text(1, 0, -2), "x^2 - 2 = 0", "Should be 'x^2 - 2 = 0'")
+        self.assertEqual(make_se_text(1, 5, 0), "x^2 + 5x = 0", "Should be 'x^2 + 5x = 0'")
+        self.assertEqual(make_se_text(4, -5, 6), "4x^2 - 5x + 6 = 0", "Should be '4x^2 - 5x + 6 = 0'")
+        self.assertEqual(make_se_text(4, 5, -6), "4x^2 + 5x - 6 = 0", "Should be '4x^2 + 5x - 6 = 0'")
 
 
 
 if __name__ == '__main__':
     unittest.main()
 
-
-
-# from se_solver import get_input_data
-
-# if get_input_data(['hghg', '1', '2', '3', '4']) != [[1, 2, 3], [4]]:
-#     raise Exception('Функция работает неверно!')
