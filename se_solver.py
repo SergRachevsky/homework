be912324f97739e500fb482227fd630333b63fd7
Serg Rachevsky <radogor@gmail.com>
2022-07-06 00:24:18 +0300

    some changes

--- se_solver.py
+++ se_solver.py
@@ -23,46 +23,60 @@ def make_sqare_equation(a, b, c):
 
     return f"{a_str}{b_str}{c_str} = 0"
 
-def calc_roots(a, b, d):
+def make_text_solution(solution):
+    print(solution)
+    exit()
+    ...
+
+def se_solve(a, b, c):
+    
+    solution = []
+    # calculate discriminant of suare equation
+    d = b**2 - 4*a*c
 
     if a == 0:
-        result = "Not a square equation (a = 0)"
+#        result = "Not a square equation (a = 0)"
+        solution.append(-1)
 
     elif d > 0:
         x1 = (-b + sqrt(d)) / (2 * a)
         x2 = (-b - sqrt(d)) / (2 * a)
-        result = f"x1 = {x1:6.2f}; x2 = {x2:6.2f}"
+#        result = f"x1 = {x1:6.2f}; x2 = {x2:6.2f}"
+        solution = (2, x1, x2)
 
     elif d == 0:
         x = -b / (2 * a)
-        result = f"x  = {x:6.2f}"
+#        result = f"x  = {x:6.2f}"
+        solution.append(1, x)
 
     else: 
-        result = "No roots"
+#        result = "No roots"
+        solution.append(0)
 
-    return result
+    return solution
 
-def se_solve(se_triad):
-    
-    a = se_triad[0]
-    b = se_triad[1] if len(se_triad) > 1 else 0
-    c = se_triad[2] if len(se_triad) > 2 else 0
+
+def print_solution(a, b, c, roots):
 
     square_equation = make_sqare_equation(a, b, c)
+    text_solution = make_text_solution(roots)
 
-    # calculate discriminant of suare equation
-    d = b**2 - 4*a*c
+#    se_triad_str = f"[{', '.join([f'{i:4}' for i in se_triad])}]"
+#    print(f"{se_triad_str:15}  ==>  {square_equation:24}  ==>  {roots}")
+    print(f"[{a:5}, {b:5}, {c:5}]  ==>  {square_equation:24}  ==>  {text_solution}")
 
-    # and calculate roots of suare equation 
-    roots = calc_roots(a, b, d)
 
-    se_triad_str = f"[{', '.join([f'{i:4}' for i in se_triad])}]"
-    print(f"{se_triad_str:15}  ==>  {square_equation:24}  ==>  {roots}")
-
+    ...
 
 
 if __name__ == "__main__":
     se_triads = get_input_data(sys.argv)
 
     for se_triad in se_triads:
-        se_solve(se_triad)
+
+        a = se_triad[0]
+        b = se_triad[1] if len(se_triad) > 1 else 0
+        c = se_triad[2] if len(se_triad) > 2 else 0
+
+        solution = se_solve(a, b, c)
+        print_solution(a, b, c, solution)
