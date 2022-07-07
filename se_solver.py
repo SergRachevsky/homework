16d13c8b58ad6f4f3d1435b8ceb854a4ec074eb3
Serg Rachevsky <radogor@gmail.com>
2022-07-07 14:35:16 +0300

    refactoring and commenting

--- se_solver.py
+++ se_solver.py
@@ -1,4 +1,17 @@
 #!/usr/bin/python3
+#
+# This script solves a regular quadratic equation. 
+# The program receives parameters of the equation as a stream of numbers from the command line:
+#
+# > se_solver 3 4 5 0 4 -4 11 22 123 45 42 42 67 1001 …
+#
+# All number are integers and some of them can be 0. 
+#
+# As result the script prints input numbers, textual representation of the equation and solution for the equation, 
+# one line per equation.
+#
+# There are also unit tests for this script located in test_se_solver.py. 
+# Description of how to run these tests and how to interpret test results could be found there or in README
 
 import sys
 from math import sqrt
@@ -9,7 +22,15 @@ RC_ONE_ROOT = 1
 RC_TWO_ROOTS = 2
 
 def get_input_data(args):
+    """ 
+    Makes triads (a, b, and c coefficients) from input numbers.
 
+    Args:
+        args (string): space separated integers
+
+    Returns:
+        list: list of lists of input numbers grouped by three
+    """
     # get arguments from command line (except sys.argv[0] which is a script name)
     args = args[1:]
 
@@ -21,14 +42,25 @@ def get_input_data(args):
     while len(args) % 3:
         args.append(0)
 
-    # split list of arguments to list of triads
-#    triads = [args[i:i+3] for i in range(0, len(args), 3)]
-    
+    # return value is list of lists of input numbers grouped by three
     return [args[i:i+3] for i in range(0, len(args), 3)]
 
 
 def make_se_text(a, b, c):
+    """
+    Makes square equation string from a, b, c coefficients.
+
+    Args:
+        a (integer): coefficient a
+        b (integer): coefficient b
+        c (integer): coefficient c
+
+    Returns:
+        string: textual representation of square equation
+    """
     # make square equation string more comfortable to read for humans
+    # (remove equation elements with coefficient equal to zero and also 
+    # correctly put minuses and pluses into the result string) 
     a_str = f"{a}x^2" if abs(a) != 1 else "x^2"
     b_str = f"{' + ' if b > 0 else ' - '}{abs(b)}x" if b != 0 else ""
     c_str = f"{' + ' if c > 0 else ' - '}{abs(c)}"  if c != 0 else ""
@@ -37,16 +69,36 @@ def make_se_text(a, b, c):
 
 
 def se_solve(a, b, c):
-    
+    """
+    Solves regular quadratic equation.
+
+    Args:
+        a (integer): coefficient a
+        b (integer): coefficient b
+        c (integer): coefficient c
+
+    Returns:
+        tuple: tuple of return code (integer), roots (list) and textual representation of solution (string)
+    """
+    # here we actually solve regular quadratic equation and also make textual representation of solution
+    # 
+    # result is tuple of:
+    #  - return code constant
+    #  - list of roots (empty for no roots and consist one of two numbers if equations has root(s))
+    #  - textual representation of solution used later for printing out
+
     roots = []
 
     # calculate discriminant of square equation
     d = b**2 - 4*a*c
 
+    # regular quadratic equation should have (a <> 0) so we make check for this
     if a == 0:
         rc = -1
         text = "Not a square equation (a=0)"
 
+    # equation has two roots if discriminant is positive number, has one root if discriminant is equal to zero and
+    # has no roots if discriminant is negative number
     elif d > 0:
         rc = 2
         x1 = (-b + sqrt(d)) / (2 * a)
@@ -68,15 +120,27 @@ def se_solve(a, b, c):
 
 
 def print_solution(a, b, c, text):
+    """
+    Prints quadratic equation and its soulution
+
+    Args:
+        a (integer): coefficient a
+        b (integer): coefficient b
+        c (integer): coefficient c
+        text (string): textual representation of solution
+    Returns:
+        None
+    """
     square_equation = make_se_text(a, b, c)
     print(f"[{a:5}, {b:5}, {c:5}]  ==>  {square_equation:24}  ==>  {text}")
 
 
 if __name__ == "__main__":
+    # very simple logic: get triads of coefficients, 
+    # then solve equation and print result for every triad in a loop
 
     se_triads = get_input_data(sys.argv)
 
     for a, b, c in se_triads:
-
         rc, roots, text = se_solve(a, b, c)
         print_solution(a, b, c, text)
--- test_se_solver.py
+++ test_se_solver.py
@@ -7,41 +7,45 @@ from se_solver import RC_NO_ROOTS, RC_NOT_A_SE, RC_ONE_ROOT, RC_TWO_ROOTS
 
 
 class TestSeSolver(unittest.TestCase):
-    """Calc tests"""    
+    """
+    Tests for se_solver.py script
+    """
+
     def test_get_input_data(self):
         """
-        make sure arguments from command line are converted into the list of triads correctly
+        Make sure arguments from command line are converted into the list of triads correctly
         """
-        self.assertEqual(get_input_data(['hghg', '1', '2', '3', '4']), [[1, 2, 3], [4, 0, 0]], "Should be [[1, 2, 3], [4, 0, 0]]")
+        self.assertEqual(get_input_data(['script name', '1', '2', '3', '4']), [[1, 2, 3], [4, 0, 0]], "Should be [[1, 2, 3], [4, 0, 0]]")
+        self.assertEqual(get_input_data(['script name', '1', '2', '3', '4', '5']), [[1, 2, 3], [4, 5, 0]], "Should be [[1, 2, 3], [4, 5, 0]]")
     
     
     def test_get_input_data_empty(self):
         """
-        no arguments should lead to empty list of triads
+        No arguments should lead to empty list of triads
         """
-        self.assertEqual(get_input_data(['hghg']), [], "Should be []")
+        self.assertEqual(get_input_data(['script name']), [], "Should be []")
 
     def test_not_a_se(self):
         """
-        if the first of the three is zero then result should be RC_NOT_A_SE
+        If the first of the three is zero then result should be RC_NOT_A_SE
         """
         self.assertEqual(se_solve(0, 4, 5)[0], RC_NOT_A_SE, "Should be -1")
 
     def test_no_roots(self):
         """
-        check that we correctly solved the equation with no roots
+        Check that we correctly solved the equation with no roots
         """
         self.assertEqual(se_solve(10, -1, 2)[0], RC_NO_ROOTS, "Should be 0")
 
     def test_one_root(self):
         """
-        check that we correctly solved the equation with one root
+        Check that we correctly solved the equation with one root
         """
         self.assertEqual(se_solve(-4, 12, -9)[0], RC_ONE_ROOT, "Should be 1")
 
     def test_two_roots(self):
         """
-        check that we correctly solved the equation with two roots
+        Check that we correctly solved the equation with two roots
         """
         self.assertEqual(se_solve(2, 5, -2)[0], RC_TWO_ROOTS, "Should be 2")
 
@@ -55,8 +59,6 @@ class TestSeSolver(unittest.TestCase):
         self.assertEqual(make_se_text(1, 5, 0), "x^2 + 5x = 0", "Should be 'x^2 + 5x = 0'")
         self.assertEqual(make_se_text(4, -5, 6), "4x^2 - 5x + 6 = 0", "Should be '4x^2 - 5x + 6 = 0'")
         self.assertEqual(make_se_text(4, 5, -6), "4x^2 + 5x - 6 = 0", "Should be '4x^2 + 5x - 6 = 0'")
-
-
 
 if __name__ == '__main__':
     unittest.main()
